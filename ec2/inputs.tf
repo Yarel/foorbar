f5776c7c4f4f02839532355a7359b2a8cbce8aef
Yarel Balcazar <yarelbalcazar@gmail.com>
2021-08-22 09:49:16 +0000

    add ec2

---
+++ ec2/inputs.tf
@@ -0,0 +1,7 @@
+variable "region" { default = "us-east-1" }
+variable "vpc_id"{ default = "vpc-b5b640c8" }
+variable "project_name"{ default = "ec2-test"}
+variable "ami"{ default = "ami-09e67e426f25ce0d7" }
+variable "instance_type"{ default = "t2.micro" }
+variable "key_name"{ default = "yarel" }
+variable "subnet_id"{ default = "subnet-6b943334" }
